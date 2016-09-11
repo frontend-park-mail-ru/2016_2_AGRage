0bd81527033a7c867361a7621b6c6f2f16e8aae2
Your Name <andreykuleb@mail.ru>
2016-09-11 15:14:57 +0300

    first commit

---
+++ js/popUp.js
@@ -0,0 +1,13 @@
+
+    $(document).ready(function(){
+        //Скрыть PopUp при загрузке страницы
+        PopUpHide();
+    });
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup1").show();
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#popup1").hide();
+    }

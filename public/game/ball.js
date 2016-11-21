2ef17ede8d11da1793b2a1176b31a57a6dcdfc14
Sany <you@example.com>
2016-11-21 12:45:48 +0300

    first unit

--- public/game/ball.js
+++ public/game/ball.js
@@ -6,11 +6,8 @@
 		/**
 		 * Конструктор класса Ball
 		 */
-		constructor ({x = 100, y = 100, r = 40, color = '#0095DD', vx = 0, vy = 0}) {
+		constructor ({x = 100, y = 280, color = '#0095DD', vx = 0}) {
 			this.vx = vx;
-			this.vy = vy;
-
-			this.r = r;
 			this.x = x;
 			this.y = y;
 
@@ -19,25 +16,27 @@
 
 		dv ({vx = 0, vy = 0}) {
 			this.vx += vx;
-			this.vy += vy;
+			//this.vy += vy;
 		}
 
 		update (dt) {
 			this.x += this.vx * dt;
-			this.y += this.vy * dt;
+			//this.y += this.vy * dt;
 		}
 
 		checkRectangleIntersection ({width, height}, action = 'relect') {
 			let result = {};
-			if (this.x + this.r > width || this.x - this.r < 0) {
+			if (this.x + 100 > width || this.x - 100 < 0) {
 				result.x = true;			}
 
-			if (this.y + this.r > height || this.y - this.r < 0) {
+			/* if (this.y + this.r > height || this.y - this.r < 0) {
 				result.y = true;
-			}
+			} */
 
 			this[action](result);
-		}
+		} 
+
+
 
 		destroy (axis) {
 			if (axis.x || axis.y) {
@@ -55,9 +54,17 @@
 
 		draw (ctx) {
 			ctx.beginPath();
-			ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+			/* ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
 			ctx.fillStyle = this.color;
 			ctx.fill();
+			ctx.closePath(); */
+
+			//var canvas = document.getElementById("canvas");
+			//var image = document.getElementById("unit1.png");
+			var image = new Image();
+			image.src = "unit1.png"; 
+
+			ctx.drawImage(image, this.x, this.y, 100, 100);
 			ctx.closePath();
 		}
 
